20190626d4c7294b3a8abed079fd385ba6f73281
JackieCalapristi <jackie.lc11@gmail.com>
2020-08-10 12:56:06 -0700

    Adding a header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 // Components
-// import Menu from "./Menu"
+import Menu from "./Menu"
 // import Hamburger from "./Hamburger"
 // import MobileMenu from "./MobileMenu"
 // import Welcome from "./Welcome"
+
 // Styles
 import { MenuWrapper, Wrapper } from "./Header.styles"
 
 const Header = ({ siteTitle = ``}) => {
-  // const [menuOpen, setMenuOpen] = useState(false);
-  // const menuItems = [
-  //   {id: 1, name: 'Resume', link: Resume }, 
-  //   {id: 2, name: 'Portfolio', link: '#portfolio'}, 
-  //   {id: 3, name: 'Contact', link: '#contact'},
-  // ];
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuItems = [
+    {id: 1, name: 'About', link: '#about' }, 
+    {id: 2, name: 'Contact', link: '#contact'},
+  ];
 
   return (
     <Wrapper>
       <MenuWrapper>
         {/* <Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <MobileMenu menuOpen={menuOpen} items={menuItems} />
-        <Menu items={menuItems} /> */}
-        I'm a header
+        <MobileMenu menuOpen={menuOpen} items={menuItems} /> */}
+        <Menu items={menuItems} /> 
+        Plantspeak 🌱
+        Talk plants to me 🌱
+        Plantucation 🌱
+        Plantopia 🌱
       </MenuWrapper>
       {/* <Welcome /> */}
     </Wrapper>
